fee50b5307a8cd31a51997e1f6eac0f573e2f18d
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-02 20:54:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 name_hash = {
-  blake: 5
+  blake: 5,
+  ashley: 2,
+  adam: 1
 }
 def key_for_min_value(name_hash)
 
